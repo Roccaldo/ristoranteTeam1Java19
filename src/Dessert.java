83c2a99e9a5ba5eae09753307b6caf7aea521393
Keoni03PA <palumbo.g.manuel@gmail.com>
2023-12-01 14:14:36 +0100

    aggiunta fix su Prodotti <public Prodotti(){}> aggiunta sottoclasse Dessert e implementata ereditarietà da Prodotti

---
+++ src/Dessert.java
@@ -0,0 +1,6 @@
+public class Dessert extends Prodotti{
+    public Dessert(){};
+    public Dessert(String portata, String nomePiatto, Double prezzo) {
+        super(portata, nomePiatto, prezzo);
+    }
+}
--- src/Prodotti.java
+++ src/Prodotti.java
@@ -6,6 +6,8 @@ public abstract class Prodotti {
 
     public Double prezzo;
 
+    public Prodotti(){};
+
     public Prodotti(String portata, String nomePiatto, Double prezzo) {
         this.portata = portata;
         this.nomePiatto = nomePiatto;
