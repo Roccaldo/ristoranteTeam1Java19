6ae66ed53857d84e67f1b16e9ae26630b5ffabfc
ChiaraG91 <149475150+ChiaraG91@users.noreply.github.com>
2023-12-01 13:48:46 +0100

    Preparazione progetto iniziale

---
+++ src/Prodotti.java
@@ -0,0 +1,47 @@
+public abstract class Prodotti {
+
+    public String portata;
+
+    public String nomePiatto;
+
+    public Double prezzo;
+
+    public Prodotti(String portata, String nomePiatto, Double prezzo) {
+        this.portata = portata;
+        this.nomePiatto = nomePiatto;
+        this.prezzo = prezzo;
+    }
+
+    public String getPortata() {
+        return portata;
+    }
+
+    public void setPortata(String portata) {
+        this.portata = portata;
+    }
+
+    public String getNomePiatto() {
+        return nomePiatto;
+    }
+
+    public void setNomePiatto(String nomePiatto) {
+        this.nomePiatto = nomePiatto;
+    }
+
+    public Double getPrezzo() {
+        return prezzo;
+    }
+
+    public void setPrezzo(Double prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    @Override
+    public String toString() {
+        return "Prodotti{" +
+                "portata='" + portata + '\'' +
+                ", nomePiatto='" + nomePiatto + '\'' +
+                ", prezzo=" + prezzo +
+                '}';
+    }
+}
