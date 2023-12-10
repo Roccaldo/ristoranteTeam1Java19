3c8a0e00a91eba421468f7a214ed621bad0c9135
gianluca <alex992palermo@gmail.com>
2023-12-10 15:56:15 +0100

    Aggiunti ArrayList per i tipi di Menù

--- src/Menu.java
+++ src/Menu.java
@@ -1,2 +1,25 @@
+import prodotti.Prodotti;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Menu {
+   private List<Prodotti> menuCarne = new ArrayList<>();
+   private List<Prodotti> menuPesce = new ArrayList<>();
+   private List<Prodotti> menuVegano = new ArrayList<>();
+   public Menu(){
+
+   }
+
+   public List<Prodotti> getMenuCarne() {
+      return menuCarne;
+   }
+
+   public List<Prodotti> getMenuPesce() {
+      return menuPesce;
+   }
+
+   public List<Prodotti> getMenuVegano() {
+      return menuVegano;
+   }
 }
