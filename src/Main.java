873fc5c48fd9d6254be60df785f3b1eda9def01f
Roccaldo <roccaldodigrisolo@gmail.com>
2023-12-19 15:34:56 +0100

    aggiunte bibite e aggiustato i metodi

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,9 @@ public class Main {
     public static void main(String[] args) {
 
         //TODO creare la classe ristrorante e spostare questa logica li dentro
-        Ristorante ristorante = new Ristorante("Team Java19","Via Sparsi Per l' Italia, 5");
+        Ristorante ristorante = new Ristorante("Team Java19", "Via Sparsi Per l' Italia, 5");
         System.out.println(ristorante);
+        System.out.println();
 
         //crea menu
         Menu menuCarne = new Menu("Monte", TipoMenuEnum.CARNE);
@@ -17,69 +18,83 @@ public class Main {
         Menu menuDessert = new Menu("dolci", TipoMenuEnum.DESSERT);
         Menu menuContorni = new Menu("Contorni", TipoMenuEnum.CONTORNI);
         Menu menuPizza = new Menu("Pizza", TipoMenuEnum.PIZZA);
-
+        Menu menuBibite = new Menu("Bibite", TipoMenuEnum.BIBITE);
 
 
         //menu carne
-        menuCarne.getMenu().add(new Antipasti("Ravioli cinesi al vapore", 3.50, new ArrayList<>(Arrays.asList(AllergeniEnum.CEREALI, AllergeniEnum.SOIA))));
-        menuCarne.getMenu().add(new Antipasti("Cornetti salati", 1.50));
+        menuCarne.aggiungiPiatto((new Antipasti("Ravioli cinesi al vapore", 3.50, new ArrayList<>(Arrays.asList(AllergeniEnum.CEREALI, AllergeniEnum.SOIA)))));
+        menuCarne.aggiungiPiatto((new Antipasti("Cornetti salati", 1.50)));
 
-        menuCarne.getMenu().add(new PrimiPiatti("Pappardelle al ragù di cinghiale", 14.76, "Classica"));
-        menuCarne.getMenu().add(new PrimiPiatti("Mezzemaniche al pesto di peperoni e basilico con salsiccia", 22.30, new ArrayList<>(Arrays.asList(AllergeniEnum.CEREALI, AllergeniEnum.UOVA)), "Integrale"));
+        menuCarne.aggiungiPiatto((new PrimiPiatti("Pappardelle al ragù di cinghiale", 14.76, "Classica")));
+        menuCarne.aggiungiPiatto((new PrimiPiatti("Mezzemaniche al pesto di peperoni e basilico con salsiccia", 22.30, new ArrayList<>(Arrays.asList(AllergeniEnum.CEREALI, AllergeniEnum.UOVA)), "Integrale")));
 
-        menuCarne.getMenu().add(new Secondi("Pollo alla campagnola", TipoCotturaEnum.BEN_COTTA, 10.0));
-        menuCarne.getMenu().add( new Secondi("Tagliata di manzo", TipoCotturaEnum.AL_SANGUE, 18.0));
-
+        menuCarne.aggiungiPiatto((new Secondi("Pollo alla campagnola", TipoCotturaEnum.BEN_COTTA, 10.0)));
+        menuCarne.aggiungiPiatto((new Secondi("Tagliata di manzo", TipoCotturaEnum.AL_SANGUE, 18.0)));
 
 
         //menu pesce
-        menuPesce.getMenu().add(new Antipasti("Antipasto di mare", 4.50));
-        menuPesce.getMenu().add(new Antipasti("Pepata di cozze", 6.40));
+        menuPesce.aggiungiPiatto((new Antipasti("Antipasto di mare", 4.50)));
+        menuPesce.aggiungiPiatto((new Antipasti("Pepata di cozze", 6.40)));
 
-        menuPesce.getMenu().add(new PrimiPiatti("Spaghetti allo scoglio", 14.0, "Classica"));
-        menuPesce.getMenu().add(new PrimiPiatti("Linguine all'astice", 9.0, "Classica"));
+        menuPesce.aggiungiPiatto((new PrimiPiatti("Spaghetti allo scoglio", 14.0, "Classica")));
+        menuPesce.aggiungiPiatto((new PrimiPiatti("Linguine all'astice", 9.0, "Classica")));
 
-        menuPesce.getMenu().add( new Secondi("Salmone croccante", TipoCotturaEnum.STANDARD, 18.0));
-        menuPesce.getMenu().add( new Secondi( "Polpettine di tonno e ricotta", TipoCotturaEnum.STANDARD, 18.0));
+        menuPesce.aggiungiPiatto((new Secondi("Salmone croccante", TipoCotturaEnum.STANDARD, 18.0)));
+        menuPesce.aggiungiPiatto((new Secondi("Polpettine di tonno e ricotta", TipoCotturaEnum.STANDARD, 18.0)));
 
         //menu vegano
-        menuVegano.getMenu().add(new Antipasti( "Insalata di ceci", 4.50));
-        menuVegano.getMenu().add(new Antipasti( "Falafel", 6.40));
+        menuVegano.aggiungiPiatto((new Antipasti("Insalata di ceci", 4.50)));
+        menuVegano.aggiungiPiatto((new Antipasti("Falafel", 6.40)));
 
-        menuVegano.getMenu().add(new PrimiPiatti( "Carbonara Vegana", 11.0, "Integrale"));
-        menuVegano.getMenu().add(new PrimiPiatti( "Lasagna vegana", 11.0, "Riso"));
+        menuVegano.aggiungiPiatto((new PrimiPiatti("Carbonara Vegana", 11.0, "Integrale")));
+        menuVegano.aggiungiPiatto((new PrimiPiatti("Lasagna vegana", 11.0, "Riso")));
 
-        menuVegano.getMenu().add( new Secondi("Strudel salato", TipoCotturaEnum.STANDARD, 18.0));
-        menuVegano.getMenu().add( new Secondi( "Seitan alla piastra", TipoCotturaEnum.STANDARD, 18.0));
+        menuVegano.aggiungiPiatto((new Secondi("Strudel salato", TipoCotturaEnum.STANDARD, 18.0)));
+        menuVegano.aggiungiPiatto((new Secondi("Seitan alla piastra", TipoCotturaEnum.STANDARD, 18.0)));
 
         //menu contorni
-        menuContorni.getMenu().add(new Contorni("Patate fritte", 1.50));
-        menuContorni.getMenu().add(new Contorni("Insalata", 1.50));
+        menuContorni.aggiungiPiatto((new Contorni("Patate fritte", 1.50)));
+        menuContorni.aggiungiPiatto((new Contorni("Insalata", 1.50)));
 
         //menu dessert
-        menuDessert.getMenu().add(new Dessert("Cheesecake di yogurt alle fragole", CalorieEnum.MEDIO, 12.99));
-        menuDessert.getMenu().add(new Dessert( "Crostata al limone meringata", CalorieEnum.ALTO, 9.99));
-        menuDessert.getMenu().add(new Dessert("Tronchetti vegani di banana al cioccolato fondente", CalorieEnum.MEDIO, 7.99));
-        menuDessert.getMenu().add(new Dessert("Frutta alle spezie vegano", CalorieEnum.BASSO, 6.99));
+        menuDessert.aggiungiPiatto((new Dessert("Cheesecake di yogurt alle fragole", CalorieEnum.MEDIO, 12.99)));
+        menuDessert.aggiungiPiatto((new Dessert("Crostata al limone meringata", CalorieEnum.ALTO, 9.99)));
+        menuDessert.aggiungiPiatto((new Dessert("Tronchetti vegani di banana al cioccolato fondente", CalorieEnum.MEDIO, 7.99)));
+        menuDessert.aggiungiPiatto((new Dessert("Frutta alle spezie vegano", CalorieEnum.BASSO, 6.99)));
 
         //menu pizza
         menuPizza.aggiungiPiatto(new Pizza("Margherita", "Integrale", 4.50));
         menuPizza.aggiungiPiatto(new Pizza("Crudaiola", "Normale", 4.50));
-        menuPizza.aggiungiPiatto(new Pizza("Vegetariana","Normale",5.00));
+        menuPizza.aggiungiPiatto(new Pizza("Vegetariana", "Normale", 5.00));
 
-        //stampa
+        //menu bibite
+        menuBibite.aggiungiPiatto(new Bibite("Coca cola", false, 1.50));
+        menuBibite.aggiungiPiatto(new Bibite("Fanta", false, 2.00));
+        menuBibite.aggiungiPiatto(new Bibite("Birra", true, 2.50));
+        menuBibite.aggiungiPiatto(new Bibite("Vino bianco", true, 14.50));
+        menuBibite.aggiungiPiatto(new Bibite("Vino Rosso", true, 14.50));
+
+        //stampa menu
+        System.out.println();
         menuCarne.stampaMenu();
+        System.out.println();
         menuPesce.stampaMenu();
+        System.out.println();
         menuVegano.stampaMenu();
+        System.out.println();
         menuContorni.stampaMenu();
+        System.out.println();
         menuDessert.stampaMenu();
+        System.out.println();
         menuPizza.stampaMenu();
+        System.out.println();
+        menuBibite.stampaMenu();
+        System.out.println();
 
         //metodo aggiungi e rimuovi piatti
         menuCarne.aggiungiPiatto(new Antipasti("Ravioli cinesi al vapore", 3.50));
         menuCarne.rimuoviPiatto(new Antipasti("Ravioli cinesi al vapore", 3.50));
+        System.out.println();
         menuCarne.stampaMenu();
-
-
     }
 }
